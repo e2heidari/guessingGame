d9a745590fd9ba350a2b93a0e031e6059772b11b
Ehsan <Ehsan.heydari@gmail.com>
2020-06-25 01:37:54 -0700

    clear the input feild with focus

--- guessingGame.js
+++ guessingGame.js
@@ -3,12 +3,11 @@ const randomNumber = Math.round(Math.random() * 100);
 function Process() {
     var RNumber = document.getElementById("yourNumber").value;
     if (randomNumber > RNumber) {
-        answer = "give me higher number"
-
+        answer = RNumber + " give me higher number"
     } else if (randomNumber < RNumber) {
-        answer = "give me lower number"
+        answer = RNumber + " give me lower number"
     } else {
-        answer = "yeep, it's right"
+        answer = RNumber + " yeep, it's right"
     }
     document.getElementById("result").innerText = answer
 }
