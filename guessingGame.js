e3f1f8024c8293a3bf17ca095b2099ba74023478
Ehsan <Ehsan.heydari@gmail.com>
2020-07-11 02:06:44 -0700

    add color to text

--- guessingGame.js
+++ guessingGame.js
@@ -1,14 +1,16 @@
 answer = ""
 const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber)
 function Process() {
     var RNumber = document.getElementById("yourNumber").value;
+    RNumber = Number(RNumber)
     if (randomNumber > RNumber) {
-        answer = RNumber + " give me higher number"
+        answer = "<span class='red'>" + RNumber + " give me higher number</span>"
     } else if (randomNumber < RNumber) {
-        answer = RNumber + " give me lower number"
+        answer = "<span class='green'>" + RNumber + " give me lower number</span>"
     } else {
-        answer = RNumber + " yeep, it's right"
+        answer = "<span class='yellow'>" + RNumber + " yeep, it's right</span>"
         document.getElementById("yourNumber").disabled = true;
     }
-    document.getElementById("result").innerText = answer
+    document.getElementById("result").innerHTML = answer
 }
